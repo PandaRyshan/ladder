a9cc797a0a99a42d9747111a6bc72cd6f6c47ebd
Hu Xiaohong <xiaohong@pandas.run>
2023-05-18 20:20:01 +0800

    fix certs dir

--- setup.sh
+++ setup.sh
@@ -189,9 +189,9 @@ function prepare_config {
 
 	sed -i "s/<your-host-ip>/$(curl -s https://ifconfig.me)/g" ./config/v2ray/config.json
 	sed -i "s/<your-v2ray-domain>/${V2RAY_SUB}.${DOMAIN}/g" ./config/v2ray/config.json
-	sed -i "s/<your-vpn-domain>/${OCSERV_SUB}.${DOMAIN}/g" ./config/haproxy/haproxy.cfg
+	sed -i "s/<your-ocserv-domain>/${OCSERV_SUB}.${DOMAIN}/g" ./config/haproxy/haproxy.cfg
 	sed -i "s/<your-v2ray-domain>/${V2RAY_SUB}.${DOMAIN}/g" ./config/haproxy/haproxy.cfg
-	sed -i "s/<your-vpn-domain>/${OCSERV_SUB}.${DOMAIN}/g" ./config/ocserv/ocserv.conf
+	sed -i "s/<your-ocserv-domain>/${OCSERV_SUB}.${DOMAIN}/g" ./config/ocserv/ocserv.conf
 
 	# download latest geoip.dat and geosite.dat to ./geodata directory
 	mkdir -p ./config/geodata
