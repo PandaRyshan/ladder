7ca4cf1b8b901419c33dbe8d674d94a8910a92c5
Hu Xiaohong <xiaohong@pandas.run>
2024-09-03 21:36:15 +0800

    fix confirm issue when install packages

--- setup.sh
+++ setup.sh
@@ -190,7 +190,7 @@ EOF
 		# Uninstall conflicting packages:
 		for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done;
 		sudo apt-get update
-		sudo apt-get install ca-certificates curl
+		sudo apt-get install -y ca-certificates curl
 		sudo install -m 0755 -d /etc/apt/keyrings
 		sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 		sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -200,11 +200,11 @@ EOF
 		$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		sudo apt-get update
-		sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+		sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 	elif [[ "${OS,,}" == *"debian"* ]]; then
 		for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 		sudo apt-get update
-		sudo apt-get install ca-certificates curl
+		sudo apt-get install -y ca-certificates curl
 		sudo install -m 0755 -d /etc/apt/keyrings
 		sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 		sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -214,7 +214,7 @@ EOF
 		$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 		sudo apt-get update
-		sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+		sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 	elif [[ "${OS,,}" == *"centos"* ]]; then
 		sudo yum remove docker \
 				  docker-client \
@@ -226,7 +226,7 @@ EOF
 				  docker-engine
 		sudo yum install -y yum-utils
 		sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-		sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin wget git uuid-runtime
+		sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin wget git uuid-runtime
 	elif [[ "${OS,,}" == *"fedora"* ]]; then
 		sudo dnf remove docker \
 				  docker-client \
@@ -238,7 +238,7 @@ EOF
 				  docker-selinux \
 				  docker-engine-selinux \
 				  docker-engine
-		sudo dnf -y install dnf-plugins-core
+		sudo dnf install -y dnf-plugins-core
 		sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 	elif [[ "${OS,,}" == *"arch"* ]]; then
 		sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin wget git uuid-runtime
